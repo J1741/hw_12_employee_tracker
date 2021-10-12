bab349d8c4614abbd42e814fc26a29b50827a9be
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-12 02:31:45 -0400

    adding seed data for role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,13 @@ VALUES ("Administration"),
   ("Customer Service"),
   ("Exploration"),
   ("Transportation");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 500000, 1),
+  ("VP", 250000, 1),
+  ("Service Manager", 80000, 2),
+  ("Service Representative", 40000, 2),
+  ("Cartographer", 150000, 3),
+  ("Navigator", 75000, 3),
+  ("Train Conductor", 60000, 4),
+  ("Truck Driver", 50000, 4);
