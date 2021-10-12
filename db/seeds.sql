eaa98f697fa213c698738c6f3e2d0e8913cc3bec
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-12 11:49:06 -0400

    adding seed data for employee table; confirmed seeding works for all tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,13 @@ VALUES ("CEO", 500000, 1),
   ("Navigator", 75000, 3),
   ("Train Conductor", 60000, 4),
   ("Truck Driver", 50000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Julius", "Caesar", 1, DEFAULT), 
+  ("Romulus", "Augustus", 2, 1), 
+  ("Leader", "Snoke", 3, 2), 
+  ("Kylo", "Ren", 4, 3), 
+  ("Leo", "Tolstoy", 5, 2), 
+  ("Anton", "Chekhov", 6, 5), 
+  ("Neil", "Armstrong", 7, 2), 
+  ("Buzz", "Aldrin", 8, 7);
