4fcde3eacef6048e5be0ec1173b4f78c61ef921c
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-12 18:24:15 -0400

    removed main() function from server.js to refactor, as it was not playing nicely with the db connection..

--- server.js
+++ server.js
@@ -19,38 +19,3 @@ db.connect((err) => {
   }
   console.log("** Connected to employee_management_db **");
 });
-
-function main() {
-  // prompt user for db action
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "dbAction",
-      message: "Welcome to the employee management system! What would you like to do?",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-        new inquirer.Separator(),
-        "Exit the application",
-        new inquirer.Separator(),
-      ],
-    }
-  ]).then(answer => {
-
-  // log selected action
-  console.log("Selected action is:", answer.dbAction);
-
-  // acknowledge exit
-  if (answer.dbAction === "Exit the application") {
-    console.log("\x1b[36m%s\x1b[0m", "\n----\nExiting .. Bye!");
-  }
-
-  });
-}
-
-// main();
