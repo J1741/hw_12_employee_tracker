3fd1e2dc3f24972c0e60b141894748a89046f60e
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-13 03:52:25 -0400

    adding view all departments feature

--- server.js
+++ server.js
@@ -20,3 +20,50 @@ db.connect((err) => {
   }
   console.log("** Connected to employee_management_db **");
 });
+
+// gets db action from user
+function main () {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "dbAction",
+      message: "Welcome to the employee management system! What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        new inquirer.Separator(),
+      ], 
+    }
+  ]).then((answer) => {
+    console.log("\x1b[36m%s\x1b[0m", `\n** Selected action: ${answer.dbAction} **\n`);
+   
+    // handle view all departments
+    if (answer.dbAction === "View all departments") {
+      viewAllDepartments();
+    }
+
+  });
+}
+
+// view all departments
+function viewAllDepartments() {
+  db.query("SELECT id,name FROM department", function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    console.table(results);
+    main();
+  });
+}
+
+// runs the app
+function init() {
+  main();
+}
+
+init();
